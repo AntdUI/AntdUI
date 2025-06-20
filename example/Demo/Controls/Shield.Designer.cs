8ff264f9a28417c329f3800c535b0995ca8c5b4e
futurebk <futurex894@qq.com>
2025-06-20 14:30:53 +0000

    🌵 Shield

---
+++ example/Demo/Controls/Shield.Designer.cs
@@ -0,0 +1,133 @@
+﻿// COPYRIGHT (C) Tom. ALL RIGHTS RESERVED.
+// THE AntdUI PROJECT IS AN WINFORM LIBRARY LICENSED UNDER THE Apache-2.0 License.
+// LICENSED UNDER THE Apache License, VERSION 2.0 (THE "License")
+// YOU MAY NOT USE THIS FILE EXCEPT IN COMPLIANCE WITH THE License.
+// YOU MAY OBTAIN A COPY OF THE LICENSE AT
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN WRITING, SOFTWARE
+// DISTRIBUTED UNDER THE LICENSE IS DISTRIBUTED ON AN "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+// SEE THE LICENSE FOR THE SPECIFIC LANGUAGE GOVERNING PERMISSIONS AND
+// LIMITATIONS UNDER THE License.
+// GITEE: https://gitee.com/AntdUI/AntdUI
+// GITHUB: https://github.com/AntdUI/AntdUI
+// CSDN: https://blog.csdn.net/v_132
+// QQ: 17379620
+
+namespace Demo.Controls
+{
+    partial class Shield
+    {
+        /// <summary> 
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary> 
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            shield1 = new AntdUI.Shield();
+            shield2 = new AntdUI.Shield();
+            shield3 = new AntdUI.Shield();
+            shield4 = new AntdUI.Shield();
+            SuspendLayout();
+            // 
+            // shield1
+            // 
+            shield1.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 134);
+            shield1.Location = new System.Drawing.Point(436, 18);
+            shield1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            shield1.Name = "shield1";
+            shield1.Padding = new System.Windows.Forms.Padding(5);
+            shield1.Radius = 0;
+            shield1.Size = new System.Drawing.Size(160, 55);
+            shield1.StatusColor = System.Drawing.Color.FromArgb(93, 176, 186);
+            shield1.StatusText = "Apache 2.0";
+            shield1.SubjectColor = System.Drawing.Color.FromArgb(84, 84, 84);
+            shield1.SubjectText = "Licenses";
+            shield1.TabIndex = 0;
+            shield1.Text = "shield1";
+            // 
+            // shield2
+            // 
+            shield2.Location = new System.Drawing.Point(13, 26);
+            shield2.Name = "shield2";
+            shield2.Padding = new System.Windows.Forms.Padding(5);
+            shield2.Size = new System.Drawing.Size(119, 38);
+            shield2.StatusColor = System.Drawing.Color.FromArgb(17, 130, 195);
+            shield2.StatusText = "V2.0.6";
+            shield2.SubjectColor = System.Drawing.Color.FromArgb(84, 84, 84);
+            shield2.SubjectText = "AntdUI";
+            shield2.TabIndex = 1;
+            shield2.Text = "shield2";
+            // 
+            // shield3
+            // 
+            shield3.Location = new System.Drawing.Point(138, 26);
+            shield3.Name = "shield3";
+            shield3.Padding = new System.Windows.Forms.Padding(5);
+            shield3.Size = new System.Drawing.Size(154, 39);
+            shield3.StatusColor = System.Drawing.Color.FromArgb(76, 205, 27);
+            shield3.StatusText = "Apache 2.0";
+            shield3.SubjectColor = System.Drawing.Color.FromArgb(84, 84, 84);
+            shield3.SubjectText = "Licenses";
+            shield3.TabIndex = 2;
+            shield3.Text = "shield3";
+            // 
+            // shield4
+            // 
+            shield4.Location = new System.Drawing.Point(309, 31);
+            shield4.Name = "shield4";
+            shield4.Padding = new System.Windows.Forms.Padding(5);
+            shield4.Radius = 0;
+            shield4.Size = new System.Drawing.Size(120, 29);
+            shield4.StatusColor = System.Drawing.Color.FromArgb(17, 130, 195);
+            shield4.StatusText = "V2.0.6";
+            shield4.SubjectColor = System.Drawing.Color.FromArgb(84, 84, 84);
+            shield4.SubjectText = "AntdUI";
+            shield4.TabIndex = 3;
+            shield4.Text = "shield4";
+            // 
+            // Shield
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            Controls.Add(shield4);
+            Controls.Add(shield3);
+            Controls.Add(shield2);
+            Controls.Add(shield1);
+            Font = new System.Drawing.Font("Microsoft YaHei UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 134);
+            Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            Name = "Shield";
+            Size = new System.Drawing.Size(653, 231);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private AntdUI.Shield shield1;
+        private AntdUI.Shield shield2;
+        private AntdUI.Shield shield3;
+        private AntdUI.Shield shield4;
+    }
+}
---
+++ src/AntdUI/Controls/Shield.cs
@@ -0,0 +1,189 @@
+﻿// COPYRIGHT (C) Tom. ALL RIGHTS RESERVED.
+// THE AntdUI PROJECT IS AN WINFORM LIBRARY LICENSED UNDER THE Apache-2.0 License.
+// LICENSED UNDER THE Apache License, VERSION 2.0 (THE "License")
+// YOU MAY NOT USE THIS FILE EXCEPT IN COMPLIANCE WITH THE License.
+// YOU MAY OBTAIN A COPY OF THE LICENSE AT
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN WRITING, SOFTWARE
+// DISTRIBUTED UNDER THE LICENSE IS DISTRIBUTED ON AN "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+// SEE THE LICENSE FOR THE SPECIFIC LANGUAGE GOVERNING PERMISSIONS AND
+// LIMITATIONS UNDER THE License.
+// GITEE: https://gitee.com/AntdUI/AntdUI
+// GITHUB: https://github.com/AntdUI/AntdUI
+// CSDN: https://blog.csdn.net/v_132
+// QQ: 17379620
+
+using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Design;
+using System.Windows.Forms;
+
+namespace AntdUI
+{
+    /// <summary>
+    /// Shield 徽章
+    /// </summary>
+    /// <remarks>展示设备信号。</remarks>
+    [Description("Shield 徽章")]
+    [ToolboxItem(true)]
+    public class Shield:IControl
+    {
+        #region 属性默认值
+        public Shield()
+        {
+            base.Padding = new Padding(5);
+            SubjectColor = Color.FromArgb(84, 84, 84);
+            StatusColor = Color.FromArgb(17, 130, 195);
+            SubjectText = "Licenses";
+            StatusText = "Apache 2.0";
+        }
+        #endregion
+
+        #region 属性
+
+        Color? subjectColor;
+        /// <summary>
+        /// 填充颜色
+        /// </summary>
+        [Description("标题颜色"), Category("外观"), DefaultValue(null)]
+        [Editor(typeof(Design.ColorEditor), typeof(UITypeEditor))]
+        public Color? SubjectColor
+        {
+            get => subjectColor;
+            set
+            {
+                if (subjectColor == value) return;
+                subjectColor = value;
+                Invalidate();
+                OnPropertyChanged(nameof(SubjectColor));
+            }
+        }
+
+
+        Color? statusColor;
+        /// <summary>
+        /// 填充颜色
+        /// </summary>
+        [Description("状态颜色"), Category("外观"), DefaultValue(null)]
+        [Editor(typeof(Design.ColorEditor), typeof(UITypeEditor))]
+        public Color? StatusColor
+        {
+            get => statusColor;
+            set
+            {
+                if (statusColor == value) return;
+                statusColor = value;
+                Invalidate();
+                OnPropertyChanged(nameof(StatusColor));
+            }
+        }
+
+        int radius = 6;
+        /// <summary>
+        /// 圆角
+        /// </summary>
+        [Description("圆角"), Category("外观"), DefaultValue(6)]
+        public int Radius
+        {
+            get => radius;
+            set
+            {
+                if (radius == value) return;
+                radius = value;
+                Invalidate();
+                OnPropertyChanged(nameof(Radius));
+            }
+        }
+        #region 文本
+
+        string? subject;
+        /// <summary>
+        /// 文本
+        /// </summary>
+        [Editor(typeof(System.ComponentModel.Design.MultilineStringEditor), typeof(UITypeEditor))]
+        [Description("标题"), Category("外观"), DefaultValue(null)]
+        public string? SubjectText
+        {
+            get => subject;
+            set
+            {
+                if (subject == value) return;
+                subject = value;
+                Invalidate();
+                OnPropertyChanged(nameof(SubjectText));
+            }
+        }
+
+        string? status;
+        /// <summary>
+        /// 文本
+        /// </summary>
+        [Editor(typeof(System.ComponentModel.Design.MultilineStringEditor), typeof(UITypeEditor))]
+        [Description("状态"), Category("外观"), DefaultValue(null)]
+        public string? StatusText
+        {
+            get => status;
+            set
+            {
+                if (status == value) return;
+                status = value;
+                Invalidate();
+                OnPropertyChanged(nameof(StatusText));
+            }
+        }
+
+        #endregion
+
+        #endregion
+        #region 渲染
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            var _rect = ClientRectangle;
+            var g = e.Graphics.High();
+            var subjectSize = g.MeasureString(SubjectText, Font);
+            subjectSize = new Size(subjectSize.Width + Padding.Size.Width, subjectSize.Height + Padding.Size.Height);
+            var statusSize = g.MeasureString(StatusText, Font);
+            statusSize = new Size(statusSize.Width + Padding.Size.Width, statusSize.Height + Padding.Size.Height);
+            var rect = new Rectangle((_rect.Width - (subjectSize.Width + statusSize.Width)) / 2, (_rect.Height - subjectSize.Height) / 2, subjectSize.Width + statusSize.Width, subjectSize.Height);
+            float _radius = radius * Config.Dpi;
+            using (var path_pain = rect.RoundPath(_radius))
+            {
+                using (var brush = new SolidBrush(StatusColor ?? Colour.Text.Get("Shield", ColorScheme)))
+                {
+                    g.Fill(brush, path_pain);
+                }
+                using (var bmp = new Bitmap(_rect.Width, _rect.Height))
+                {
+                    using (var g2 = Graphics.FromImage(bmp).High())
+                    {
+                        Color _color = SubjectColor?? Colour.Text.Get("Shield", ColorScheme);
+                        g2.Fill(_color, path_pain);
+                        g2.CompositingMode = System.Drawing.Drawing2D.CompositingMode.SourceCopy;
+                        g2.Fill(Brushes.Transparent, new RectangleF(rect.X + subjectSize.Width, 0, rect.Width, bmp.Height));
+                    }
+                    g.Image(bmp, _rect);
+                }
+                var subjectRect = new Rectangle(rect.X + Padding.Left, rect.Y + Padding.Top, subjectSize.Width - Padding.Size.Width, subjectSize.Height - Padding.Size.Height);
+                using (var brush = new SolidBrush(Color.White))
+                {
+                    g.String(SubjectText, Font, brush, subjectRect);
+                }
+
+                var statusRect = new Rectangle(rect.X + subjectSize.Width + Padding.Left, rect.Y + Padding.Top, statusSize.Width - Padding.Size.Width, statusSize.Height - Padding.Size.Height);
+                using (var brush = new SolidBrush(Color.White))
+                {
+                    g.String(StatusText, Font, brush, statusRect);
+                }
+            }
+            this.PaintBadge(g);
+            base.OnPaint(e);
+        }
+    }
+
+    #endregion
+
+}
