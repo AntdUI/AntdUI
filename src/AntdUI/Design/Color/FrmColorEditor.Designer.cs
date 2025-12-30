1405b7580d4519d1b1b62e4f1c16f1b1d1e4a125
Tom <17379620@qq.com>
2025-12-30 13:45:40 +0800

    🦄 Beautiful AntdUI

--- src/AntdUI/Design/Color/FrmColorEditor.Designer.cs
+++ src/AntdUI/Design/Color/FrmColorEditor.Designer.cs
@@ -1,3 +1,9 @@
+// Copyright (C) Tom <17379620>. All Rights Reserved.
+// AntdUI WinForm Library | Licensed under Apache-2.0 License
+// Gitee: https://gitee.com/AntdUI/AntdUI
+// GitHub: https://github.com/AntdUI/AntdUI
+// GitCode: https://gitcode.com/AntdUI/AntdUI
+
 namespace AntdUI.Design
 {
     partial class FrmColorEditor
